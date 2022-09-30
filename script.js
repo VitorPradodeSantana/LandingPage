264727f983b53f582832ef2c7eee2077594502a0
VitorPradodeSantana <vitor_shotokan@hotmail.com>
2022-09-30 17:23:15 -0300

    Terminado o Carousel e com isso concluído o projeto da LandinPage

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 2,
+  freeMode: true,
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+    el: '.swiper-pagination',
+    clickable: true
+  }
+})
